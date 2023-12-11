0015136832ebdf6da654a3783aabc19793eb271a
sx0354 <sxrtiger@hotmail.com>
2023-12-11 15:08:02 +0800

    success is diffcute

--- Descartes.py
+++ Descartes.py
@@ -30,3 +30,37 @@ dirname, filenmae = os.path.split('/home/luciano/.ssh/idrsa.pub')
 
 print(filenmae)
 print(dirname)
+
+a,b,*rest = range(6)
+print(a,b,rest)
+
+print("--------------------------------------------")
+
+metro_areas = [
+    ('Tokyo','JP',36.933,(35.689722,139.691667)),
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386))
+]
+
+print('{:15} | {:^9} | {:^9}'.format('','lat.','long.'))
+fmt = '{:15} | {:9.4f} | {:9.4f}'
+for name,cc,pop,(latitude,longitude) in metro_areas:
+    if longitude <= 0:
+        print(fmt.format(name,latitude,longitude))
+
+print('-------------------------------------------')
+
+from collections import namedtuple
+
+City = namedtuple('City','name country population coordinates')
+Tokyo = City('Tokyo','JP',36.33,(35.68,139.69))
+print(Tokyo)
+print(Tokyo.population)
+print(City._fields)
+
+LatLong = namedtuple('LatLong','lat long')
+delhi_data = ('Delhi NCR','IN',21.935,LatLong(28.613,77.2))
+delhi = City._make(delhi_data)
+print(delhi)
+print(delhi._asdict())
