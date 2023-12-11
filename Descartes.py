36c128603bda207007c17a9f85f097d1ca53c487
sx0354 <sxrtiger@hotmail.com>
2023-12-11 15:39:29 +0800

    this is a A

--- Descartes.py
+++ Descartes.py
@@ -64,3 +64,88 @@ delhi_data = ('Delhi NCR','IN',21.935,LatLong(28.613,77.2))
 delhi = City._make(delhi_data)
 print(delhi)
 print(delhi._asdict())
+
+s = 'bicycle'
+print(s[1:4:2])
+print(s[::-1])
+print(s[::-3])
+
+invoice = """
+... 0.....6................................40........52...55........
+... 1909  Pimoroni PiBrella                    $17.50    3    $52.50
+... 1489  6mm Tactile Switch x20                $4.95    2     $9.90
+... 1510  Panavise Jr. - PV-201                $28.00    1    $28.00
+... 1601  PiTFT Mini Kit 320x240               $34.95    1    $34.95
+..."""
+
+SKU = slice(0,6)
+DESCRIPTION = slice(6,40)
+UNIT_PRICE = slice(40,52)
+QUANTITY = slice(52,55)
+ITEM_TOTAL = slice(55,None)
+
+line_items = invoice.split('\n')[2:]
+
+for item in line_items:
+    print(item[UNIT_PRICE],item[DESCRIPTION])
+
+l = list(range(10))
+print(l)
+print(l[2:5])
+l[2:5] = [20,30]
+
+print(l)
+print(l[5:8])
+l[5:8] = [66,77]
+print(l)
+
+print('------------------------------')
+board = [['_'] * 3 for i in range(3)]
+print(board)
+board[1][2] = 'X'
+print(board)
+
+row = ['_'] * 3
+print(row)
+print('*******')
+board = []
+for i in range(3): # board 包含 3个指向row的引用
+    board.append(row)
+print(board)
+board[1][2] = '0'
+print(board)
+
+print("------")
+board = []
+for i in range(3):
+    row = ['_'] * 3
+    board.append(row)
+
+print(board)
+board[1][2] = 'N'
+print(board)
+
+l = [1,2,3]
+print(l,id(l))
+l *= 2
+print(l,id(l))
+
+t = (1,2,3)
+print(t,id(t))
+t *= 2
+print(t, id(t))
+
+t = (1,2,[30,40])
+#t[2] += [50,60]
+print(t)
+
+fruits = ['grape', 'raspberry', 'apple', 'banana']
+print(sorted(fruits))
+print(fruits)
+
+print(fruits.sort())
+print(fruits)
+
+print(sorted(fruits,reverse=True))
+print(sorted(fruits,key=len))
+print(sorted(fruits,key=len,reverse=True))
